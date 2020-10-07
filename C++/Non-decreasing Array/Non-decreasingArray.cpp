fcb51d5450a60321b4301fbf25fca36600a28b45
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-10-07 20:20:56 +0200

    Problem Solving

--- C++/Non-decreasing Array/Non-decreasingArray.cpp
+++ C++/Non-decreasing Array/Non-decreasingArray.cpp
@@ -5,55 +5,79 @@
 
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     bool checkPossibility(vector<int> &nums)
+//     {
+//         set<int> Set_i;
+//         set<int> Set_j;
+//         int counter = 0;
+//         int corupted_num = 0;
+//         int error = 0;
+//         int j;
+//         int old = nums.size();
+
+//         for (int i = 0; i < (nums.size() - 1); i++)
+//         {
+//             for (j = i + 1; j < nums.size(); j++)
+//             {
+//                 if (nums[i] > nums[j])
+//                 {
+//                     Set_i.insert(i);
+//                     Set_j.insert(j);
+//                     if (Set_j.size() != Set_i.size())
+//                     {
+//                         Set_i.erase(i);
+//                     }
+//                     else
+//                     {
+//                         error++;
+//                         break;
+//                     }
+
+//                 }
+//             }
+//             error = 0;
+//         }
+//         Set_i.insert(nums.size());
+//         Set_j.insert(nums.size());
+//         if (Set_i.size() > 2)
+//         {
+//             return false;
+//         }
+
+//         return true;
+//     }
+// };
+
 class Solution
 {
 public:
     bool checkPossibility(vector<int> &nums)
     {
-        set<int> Set_i;
-        set<int> Set_j;
-        int counter = 0;
-        int corupted_num = 0;
-        int error = 0;
-        int j;
-        int old = nums.size();
-
-        for (int i = 0; i < (nums.size() - 1); i++)
+        int cnt = 1, n = nums.size();
+        for (int i = 1; i < n; ++i)
         {
-            for (j = i + 1; j < nums.size(); j++)
+            if (nums[i] < nums[i - 1])
             {
-                if (nums[i] > nums[j])
-                {
-                    Set_i.insert(i);
-                    Set_j.insert(j);
-                    if (Set_j.size() != Set_i.size())
-                    {
-                        Set_i.erase(i);
-                    }
-                    else
-                    {
-                        error++;
-                        break;
-                    }
+                if (cnt == 0)
+                    return false;
+                if (i == 1 || nums[i] >= nums[i - 2])
+                    nums[i - 1] = nums[i];
+                else
+                    nums[i] = nums[i - 1];
 
-                }
+                --cnt;
             }
-            error = 0;
         }
-        Set_i.insert(nums.size());
-        Set_j.insert(nums.size());
-        if (Set_i.size() > 2)
-        {
-            return false;
-        }
-
         return true;
     }
 };
 
 int main()
 {
-    // vector<int> vec{5, 7, 1, 8};
+    vector<int> vec{5, 7, 1, 8};
     // vector<int> vec{4, 2, 3};
     // vector<int> vec{4, 2, 1};
 
