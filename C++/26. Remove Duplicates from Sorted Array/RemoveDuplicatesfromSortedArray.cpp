b6fdb11a50b9746cb41e6df8e65b5a9bf2a70cfa
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-11-15 00:19:23 +0200

    .

--- C++/26. Remove Duplicates from Sorted Array/RemoveDuplicatesfromSortedArray.cpp
+++ C++/26. Remove Duplicates from Sorted Array/RemoveDuplicatesfromSortedArray.cpp
@@ -7,15 +7,34 @@ class Solution
 public:
     int removeDuplicates(vector<int> &nums)
     {
+        if (nums.size() == 0 )return 0;
+        int size = 0;
+        int All_size = nums.size();
+        auto it = nums.begin();
+        bool clear = false;
+        for (int i = 0; i < nums.size() -1 ; i++)
+        {
+          
+            if (nums[i] == nums[i + 1])
+            {
+                size++;
+                nums.erase(it + i);
+                i--;
+            }
+        }
+
+        return All_size - size;
     }
 };
 
+
 int main()
 {
-    vector<int> vec;
+    vector<int> vec{0,0,1,1,1,2,2,3,3,4};
 
     Solution s;
-    cout << "New size : " << s.removeDuplicates(vec) << endl;
+    s.removeDuplicates(vec);
+    // cout << "New size : " << s.removeDuplicates(vec) << endl;
 
     for (int i = 0; i < vec.size(); i++)
     {
@@ -26,3 +45,11 @@ int main()
 
     return 0;
 }
+
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         return unique(nums.begin(),nums.end()) - nums.begin();
+//     }
+// };
